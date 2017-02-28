e112d5774cbf0977a024e3a08db94d1da7f4a107
Ayrton Saito <ton.unesp@gmail.com>
2017-02-28 20:00:36 -0300

    Auto Deploy By Jenkins

--- src/main/java/br/com/tonsaito/mew/controller/TestController.java
+++ src/main/java/br/com/tonsaito/mew/controller/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
 
 	@RequestMapping({"/test", "hi"})
 	public String test(Model model){
-		model.addAttribute("message", "Maven + Spring Example Webapp Up and Running 222");
+		model.addAttribute("message", "Maven + Spring Example Webapp Up and Running 555");
 		return "test/index";
 	}
 	
